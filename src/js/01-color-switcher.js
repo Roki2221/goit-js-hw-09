ac46e23659c08138c9df68bb3f0319eada9253db
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-10-09 19:16:07 +0300

    first task complite

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
-const sda = 1;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let timerId = null;
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+stopButton.setAttribute('disabled', 'true');
+const startClick = () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  stopButton.removeAttribute('disabled');
+  startButton.setAttribute('disabled', 'true');
+};
+
+const stopClick = () => {
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', 'true');
+
+  clearInterval(timerId);
+};
+
+startButton.addEventListener('click', startClick);
+stopButton.addEventListener('click', stopClick);
